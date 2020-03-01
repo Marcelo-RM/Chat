d5d6353295f1029c014f9022158759e205ff42e4
Marcelo Rodrigues Montalvão <36489721+Marcelo-RM@users.noreply.github.com>
2020-03-01 14:35:29 -0300

    master files

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+
+  runApp(MyApp());
+
+//  Firestore.instance.collection("mensagens").document().setData({
+//    "text": "Olá",
+//    "from": "Marcelo",
+//    "read": false
+//  });
+//    QuerySnapshot snapshot = await Firestore.instance.collection("mensagens").getDocuments();
+//    snapshot.documents.forEach((d) {
+//      print(d.data);
+//      d.reference.updateData({"read": false});
+//    });
+
+  Firestore.instance.collection("mensagens").snapshots().listen((snaps) {
+    snaps.documents.forEach((doc) {
+      print(doc.data);
+    });
+  });
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
