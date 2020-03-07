4904c893bbffe922e72b9a133c3c97bde94b3b49
Marcelo Rodrigues Montalvão <36489721+Marcelo-RM@users.noreply.github.com>
2020-03-07 17:17:18 -0300

    listando msgs enviadas

--- lib/chatScreen.dart
+++ lib/chatScreen.dart
@@ -1,4 +1,7 @@
-import 'package:chat/testComposer.dart';
+import 'dart:io';
+import 'package:chat/textComposer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import "package:flutter/material.dart";
 
 class ChatScreen extends StatefulWidget {
@@ -7,14 +10,64 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreen extends State<ChatScreen> {
+
+  void _sendMessage({String text, File imgFile}) async {
+
+    Map<String, dynamic> data = {};
+
+    if(imgFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance.ref().child(
+        DateTime.now().millisecondsSinceEpoch.toString()
+      ).putFile(imgFile);
+
+      StorageTaskSnapshot taskSnapshot = await task.onComplete;
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data['imgUrl'] = url;
+    } 
+
+    if(text != null) data['text'] = text;
+
+    Firestore.instance.collection("mensagens").add(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-         title: Text("Olá"),
-         elevation: 0,
-       ),
-       body: TextComposer(),
+      appBar: AppBar(
+        title: Text("Olá"),
+        elevation: 0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: StreamBuilder(
+              stream: Firestore.instance.collection('mensagens').snapshots(),
+              builder: (BuildContext context, AsyncSnapshot snapshot){
+                switch(snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator()
+                    );
+                  default:
+                    List<DocumentSnapshot> documents = snapshot.data.documents.reversed.toList();
+
+                    return ListView.builder(
+                      itemCount: documents.length,
+                      reverse: true,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(documents[index].data['text']),
+                        );
+                      },
+                    );
+                }
+              },
+            ),
+          ),
+          TextComposer(_sendMessage),
+        ]
+      )
     );
   }
 }
--- lib/textComposer.dart
+++ lib/textComposer.dart
@@ -1,13 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class TextComposer extends StatefulWidget {
+  TextComposer(this.sendMessage);
+  final Function({String text, File imgFile}) sendMessage;
+
   @override
   _TextComposerState createState() => _TextComposerState();
 }
 
 class _TextComposerState extends State<TextComposer> {
-
+  final TextEditingController _controller = TextEditingController();
   bool _isComposer = false;
+  void _reset() {
+    _controller.clear();
+    setState(() {
+      _isComposer = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,27 +29,32 @@ class _TextComposerState extends State<TextComposer> {
         children: <Widget>[
           IconButton(
             icon: Icon(Icons.photo_camera),
-            onPressed: () {
-
+            onPressed: () async {
+              final File imgFile = await ImagePicker.pickImage(source: ImageSource.camera);
+              if(imgFile == null) return;
+              widget.sendMessage(imgFile: imgFile);
             },
           ),
           Expanded(
             child: TextField(
+              controller: _controller,
               decoration: InputDecoration.collapsed(hintText: "Enviar uma mensagem"),
-              onChanged: (text){
+              onChanged: (text) {
                 setState(() {
                   _isComposer = text.isNotEmpty;
                 });
               },
-              onSubmitted: (text){
-
+              onSubmitted: (text) {
+                widget.sendMessage(text: text);
+                _reset();
               },
             )
           ),
           IconButton(
-            icon: Icon(Icons.send), 
-            onPressed: _isComposer ?(){
-
+            icon: Icon(Icons.send),
+            onPressed: _isComposer ? () {
+              widget.sendMessage(text: _controller.text);
+              _reset();
             } : null
           )
         ],
